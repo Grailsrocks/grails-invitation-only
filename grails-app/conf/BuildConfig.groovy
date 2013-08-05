06132eff06d05d9af83a1c3dfa567198967c8695
Shashank Agrawal <shashank.agrawal@causecode.com>
2013-08-05 16:56:32 +0530

    Added group id to the plugin desriptor.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,13 +1,9 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir	= "target/test-reports"
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
     inherits( "global" ) {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
     }
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     repositories {
@@ -20,10 +16,12 @@ grails.project.dependency.resolution = {
     plugins {
         runtime(':asynchronous-mail:1.0-RC3') {
             excludes 'hibernate'
-         }
+        }
         compile ":fields:1.3"
         runtime ":hibernate:$grailsVersion"
         compile ":mail:1.0.1"
-        build ":tomcat:$grailsVersion"
+        build(":tomcat:$grailsVersion", ":release:2.0.3", ":rest-client-builder:1.0.2") {
+            export = false
+        }
     }
 }
